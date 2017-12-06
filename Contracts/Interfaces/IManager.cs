bea069ea79d4aa72694362537c771b2e359d4e85
Антон Кудрин <antonkudrin25@gmail.com>
2017-12-06 19:57:38 +0300

    Added Contracts Library. Implemented Manager Interface

---
+++ Contracts/Interfaces/IManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contracts.Interfaces
+{
+    public interface IManager
+    {
+        int ManagerID { get; set; }
+        string FirstName { get; set; }
+        string SecondName { get; set; }
+    }
+}
