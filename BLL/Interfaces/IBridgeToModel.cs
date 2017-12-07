cd1d51a431de046f8b326efb3952db3f66341143
Антон Кудрин <antonkudrin25@gmail.com>
2017-12-07 19:38:02 +0300

    Added IBridgeToModel

---
+++ BLL/Interfaces/IBridgeToModel.cs
@@ -0,0 +1,17 @@
+﻿using BLL.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Interfaces
+{
+    public interface IBridgeToModel
+    {
+        void AddSale(SaleDTO sale);
+        void AddSales(ICollection<SaleDTO> sales);
+        void CheckManager(ManagerDTO manager);
+        void Dispose();
+    }
+}
