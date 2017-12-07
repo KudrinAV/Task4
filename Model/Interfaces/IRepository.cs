79f6d1de75b1ce63b89262b3fee1a1192d427610
Антон Кудрин <antonkudrin25@gmail.com>
2017-12-07 18:09:47 +0300

    Implemented manager repository

--- Model/Interfaces/IRepository.cs
+++ Model/Interfaces/IRepository.cs
@@ -9,8 +9,8 @@ namespace Model.Interfaces
     public interface IRepository<T> where T : class
     {
         IEnumerable<T> GetAll();
-        T get(int id);
-        IEnumerable<T> Find();
+        T Get(int id);
+        IEnumerable<T> Find(Func<T, Boolean> predicate);
         void Create(T item);
         void Update(T item);
         void Delete(int id);
