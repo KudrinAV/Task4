d3babd012d86cd019c0c8aa6991d94d90ea2d05b
Антон Кудрин <antonkudrin25@gmail.com>
2017-12-11 00:52:03 +0300

    Added new entity to DB

--- Model/Entities/Manager.cs
+++ Model/Entities/Manager.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Model.Entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -11,9 +12,9 @@ namespace Model.DataModel
     {
         [Key]
         public int ManagerID { get; set; }
-        public string FirstName { get; set; }
         public string LastName { get; set; }
 
+        public ICollection<Report> Reports { get; set; }
         public ICollection<Sale> Sales { get; set; }
     }
 }
--- Model/Entities/Sale.cs
+++ Model/Entities/Sale.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,13 +9,13 @@ namespace Model.DataModel
 {
     public class Sale
     {
+        [Key]
         public int SaleID { get; set; }
         public DateTime Date { get; set; }
         public string Client { get; set; }
         public string Product { get; set; }
         public double Price { get; set; }
-
-        public string ManagerName { get; set; }
+        
         public Manager Manager { get; set; }
     }
 }
